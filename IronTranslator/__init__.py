12369f73863ee6824700b7b9082c95867f7206e7
ABDELMOULA <m.css933@gmail.com>
2021-04-01 13:12:03 +0200

    Add files via upload

---
+++ IronTranslator/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 31 20:59:08 2021
+
+@author: M.ABDELMOULA
+"""
+
+from IronTranslator.main import Translator
+from IronTranslator.constants import LANGUAGES, LANGUAGES_NAME, MAIN_LINK
